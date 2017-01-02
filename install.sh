11ef6ce27abd46d0805b4f6e366ed34a02df82f4
wikibootup <wikibootup@gmail.com>
2017-01-02 18:20:38 +0900

    add install.sh : brew list

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+brew install caskroom/cask/brew-cask
+brew cask update
+brew upgrade
+brew cask install slack
+brew cask install alfred
+brew cask install sourcetree
+brew cask install postman
+brew cask install iterm2
